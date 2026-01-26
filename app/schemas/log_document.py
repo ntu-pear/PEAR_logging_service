286b4cbf6093d9c294d764883bc4a18c00b0c37e
Ang Jo Wee <105429791+joweeeee09@users.noreply.github.com>
2026-01-26 16:37:56 +0800

    Revert "feat: add get_params_by_activity"

--- app/schemas/log_document.py
+++ app/schemas/log_document.py
@@ -7,7 +7,7 @@ class LogDocument(BaseModel):
     table: str
     user: str
     user_full_name: str
-    entity_id: Optional[int] = None
+    patient_id: Optional[int] = None
     original_data: Optional[dict] = None
     updated_data: Optional[dict] = None
     message: str
