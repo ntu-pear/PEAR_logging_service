5152ec1539aa4b68b466ed0054321652550a223a
Jo Wee Ang <angjowee@gmail.com>
2026-01-26 16:30:45 +0800

    feat: add get_params_by_activity

--- app/schemas/log_document.py
+++ app/schemas/log_document.py
@@ -7,7 +7,7 @@ class LogDocument(BaseModel):
     table: str
     user: str
     user_full_name: str
-    patient_id: Optional[int] = None
+    entity_id: Optional[int] = None
     original_data: Optional[dict] = None
     updated_data: Optional[dict] = None
     message: str
