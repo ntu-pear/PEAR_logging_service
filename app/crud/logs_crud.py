450c56e15d6c52800cc8df1cfddb675800504499
Jo Wee Ang <angjowee@gmail.com>
2026-03-10 00:40:49 +0800

    update match phrase for is_system_config

--- app/crud/logs_crud.py
+++ app/crud/logs_crud.py
@@ -44,7 +44,7 @@ def get_logs_by_param_patient(query: LogQuery, pageNo: int = 0, pageSize: int = 
     must_conditions.append({
         "bool": {
             "must_not": [
-                {"term": {"is_system_config": True}}
+                {"match_phrase": {"message": f"\"is_system_config\": False"}}
             ]
         }
     })
